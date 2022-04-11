133cb086c9a60c06596371e7240125d3d7b7217b
Wimull <aninhasilva@tutanota.com>
2022-04-11 16:11:38 -0300

    feat: Fonte padrão, pagina de favoritos e descobrir

---
+++ pages/descobrir/[city].tsx
@@ -0,0 +1,6 @@
+import { useRouter } from "next/router";
+export default function Descobrir() {
+  const router = useRouter();
+  const city = router.query.city;
+  return <h1>Descobrir, {city}</h1>;
+}
